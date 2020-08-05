196246ff5c14751a4733dac1c3b0f5f3cd378276
Yuuki Mori <wrong.int1109@gmail.com>
2020-08-05 12:08:38 +0000

    ベンチ回す前にやることをまとめたシェルスクリプトを追加

---
+++ pre_bench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+# from: https://gist.github.com/asflash8/0cbb743fd23385f32b412c908959a032
+set -ex
+
+#if [ -f /var/lib/mysql/mysqld-slow.log ]; then
+#    sudo mv /var/lib/mysql/mysqld-slow.log /var/lib/mysql/mysqld-slow.log.$(date "+%Y%m%d_%H%M%S")
+#fi
+if [ -f /var/log/nginx/access.log ]; then
+    sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$(date "+%Y%m%d_%H%M%S")
+fi
+sudo systemctl restart mysql
+sudo systemctl restart niita_python
+sudo systemctl restart nginx
